89d69a229218334b4e493f2ff0ba12a772eebedf
Fabian <fabian.andersson2005@gmail.com>
2025-10-25 20:42:23 +0200

    added usb (depricated) konfigs and dts support, removed icm20948 support (broken)

--- src/main.c
+++ src/main.c
@@ -1,17 +1,13 @@
-/*
- * Copyright (c) 2016 Intel Corporation
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-#include <stdio.h>
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
 
+LOG_MODULE_REGISTER(main);
 
 int main()
 {
+	LOG_INF("Hello from native_posix!");
 	while (1) {
+		LOG_INF("Run");
 		k_msleep(10);
 	}
 	return 0;
